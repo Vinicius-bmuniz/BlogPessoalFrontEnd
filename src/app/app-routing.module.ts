b5118f6c2102af20a5dfe16c13d9bc531e39a656
Vinicius Barbosa <vinicius.barbos27@gmail.com>
2022-06-30 19:17:04 -0300

    rotas criadas e adicionadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, //Esse serve para redirecionar caso o componente acessado não seja encontrado.
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
